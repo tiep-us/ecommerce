fa0d7c7866ab110f7c9890f1274bd835680eeacf
Cytimy <71780230+harumuki@users.noreply.github.com>
2020-12-17 19:37:43 +0700

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,6 +6,12 @@ module.exports = () => ({
   resolve: {
     alias: {
       './html/AccountForm.html': path.resolve(__dirname, 'template/js/custom-js/html/AccountForm.html')
+    }
+  }
+})
+module.exports = () => ({
+  resolve: {
+    alias: {
       './html/TheCard.html': path.resolve(__dirname, 'template/js/custom-js/html/TheCard.html')
     }
   }
