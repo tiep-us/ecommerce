2aa85500c9c5c4e3666b0e99e469f07114064622
Tiep <50001808+tiepdotme@users.noreply.github.com>
2020-12-18 07:51:02 +0700

    big update core form html

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -9,6 +9,11 @@ module.exports = () => ({
       './html/TheCard.html': path.resolve(__dirname, 'template/js/custom-js/html/TheCard.html'),
       './html/CartQuickview.html': path.resolve(__dirname, 'template/js/custom-js/html/CartQuickview.html'),
       './html/InstantSearch.html': path.resolve(__dirname, 'template/js/custom-js/html/InstantSearch.html'),
+      './html/AccountAddresses.html': path.resolve(__dirname, 'template/js/custom-js/html/AccountAddresses.html'),
+      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html'),
+      './html/CartItem.html': path.resolve(__dirname, 'template/js/custom-js/html/CartItem.html'),
+      './html/DiscountApplier.html': path.resolve(__dirname, 'template/js/custom-js/html/DiscountApplier.html'),
+      './html/InputPhone.html': path.resolve(__dirname, 'template/js/custom-js/html/InputPhone.html'),
       './html/AddressForm.html': path.resolve(__dirname, 'template/js/custom-js/html/AddressForm.html')
     }
   }
